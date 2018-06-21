384ae2b92cac0c2522d9c8fe665ec5348333d631
fadikamohamed <fadika.mohamed02@gmail.com>
2018-06-21 10:19:37 +0200

    Exercice 5 vraiment terminé cette fois

---
+++ fonction.js
@@ -0,0 +1,25 @@
+//Délaration de la fonction.
+function numbers() {
+  //Déclaration des variables.
+  var firstNumber = document.getElementById('firstNumber').value;
+  var secondNumber = document.getElementById('secondNumber').value;
+  //Délaration du régex et intégration dans une variable.
+  var regex = /^[0-9\.]+$/;
+  //Déclaration de la condition
+  if ((regex.test(firstNumber) == true) && (regex.test(secondNumber) == true))
+   {
+    //Conversion de la chaine de caractère en nombre entier et intégration dans une variable.
+    var firstNumberInt = parseInt(firstNumber);
+    //Conversion de la chaine de caractère en nombre décimal et intégration dans une variable.
+    var secondNumberInt = parseFloat(secondNumber);
+    //Multiplication des deux nombres obtenu.
+    var result = firstNumberInt * secondNumberInt;
+    //Boite de dialogue avec appel des variables.
+    alert(firstNumber + ' + ' + secondNumber + ' = ' + result);
+   }else
+    {
+    //Boite de dialogue avec message d'érreur.
+    alert('Entré invalide !');
+    }
+
+}
